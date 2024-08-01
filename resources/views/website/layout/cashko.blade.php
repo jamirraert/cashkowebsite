df82cc6f4ef6aaf6c13f2dff42d4e7140779092c
tony <antonio.comique@ruralnet.ph>
2024-08-01 17:12:28 +0800

    update

--- resources/views/website/layout/cashko.blade.php
+++ resources/views/website/layout/cashko.blade.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" sizes="32x32" href="/images/favicon.png" type="image/png">
     <title>Cashko Insurance</title>
-
-    
     <!-- Boxicons CDN-->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     
